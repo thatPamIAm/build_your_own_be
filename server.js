c97d6a084f8191d22626cfd884b355f7ca87eba2
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-17 12:04:40 -0600

    add all four get endpoints. add 1 post endpoint for merchants

--- server.js
+++ server.js
@@ -12,15 +12,63 @@ app.get('/', (request, response) => {
   response.send('It\'s a BYOB kind of project.')
 })
 
-app.get('/account', function(request, response) {
-  request({
-    uri: 'https://rest.viglink.com/api',
-    headers: 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
+
+app.get('/api/v1/merchants', (request, response) => {
+  database('merchants').select()
+    .then(merchants => {
+      response.status(200).json(merchants);
+    })
+    .catch(error => console.error('error: ', error))
+  })
+
+app.get('/api/v1/products', (request, response) => {
+  database('products').select()
+    .then(products => {
+      response.status(200).json(products);
+    })
+    .catch(error => console.error('error: ', error))
+})
+
+app.get('/api/v1/merchants/:merchant_id', (request, response) => {
+  database('merchants').where('merchant_id', request.params.merchant_id).select()
+    .then(folders => {
+      response.status(200).json(folders);
+    })
+    .catch(error => console.error('error: ', error))
+});
+
+app.get('/api/v1/products/:id', (request, response) => {
+  database('products').where('id', request.params.id).select()
+    .then(products => {
+      response.status(200).json(products);
+    })
+    .catch(error => console.error('error: ', error))
+})
+
+app.post('/api/v1/merchants', (request, response) => {
+  const { merchant_name, merchant_id } = request.body
+
+  database('merchants').insert({merchant_name, merchant_id}, ['merchant_name', 'merchant_id'])
+  .then(() => {
+    database('merchants').select()
+    .then(merchants => { console.log(merchants)
+      response.status(201).json(merchants)
+    })
   })
 })
 
-//rest.viglink.com/api/metrics?startDate=2016-01-01&endDate=2016-01-31'
+
+
+//app.delete('api/v1/merchants/:id')
+//app.delete('/api/v1/products/:id')
+
+//app.patch
+//app.put
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
