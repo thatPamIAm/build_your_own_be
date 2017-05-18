0e6f45813e3f845efa9e8adafb677665083af53b
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-18 13:54:58 -0600

    add products object

---
+++ products.js
@@ -0,0 +1,234 @@
+module.exports = [
+  {
+    product_keyword: 'Paprika (Smoked) 5Lb Paprika (Smoked) : GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Savory (Whole) 10Lb Savory,Whole: GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Apple Pectin (Pyrun malus) 4 oz: C',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Coconut Creme Instant Pudding 15 lb: GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'K-Cups Cafe Mocha 6/12Ct Cafe Mocha K-Cups: GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Cilantro 10Lb Cilantro: GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Granulated Garlic 25Lb Gran Garlic (Us/China Mix) : GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Garlic, Granulated (Roasted) 5 lb: GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Crispy Toasted Onion Bits 22Lb Crispy Toasted Onion Bits: GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Star Anise 5Lb Star Anise: GR',
+    merchant: '123519'
+  },
+  {
+    product_keyword: 'Febreze Unstopables Scented Candle Air Freshener Lush - 4.3 oz.',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Off! Triple Wick Citronella Candle - 23 oz.',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Glade PlugIns Scented Oil Refills Vanilla Passion Fruit - 2 ea',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Wally\'s Natural All-Natural Candle, Beeswax - 2 ea',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Mrs. Meyer\'s Clean Day Soy Candle Lemon Verbena - 7.2 oz.',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Febreze Unstopables Scented Candle Air Freshener Shimmer - 4.3 oz.',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Walgreens Cool Moisture Humidifier Filter W889-WGN',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Black &amp; Decker Travel Humidifier - 1 ea',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'BabyMoov Digital Humidifier Medium - 1 ea',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Crane USA Ultrasonic Cool Mist Humidifier 1 Gallon - 1 ea',
+    merchant: '124292'
+  },
+  {
+    product_keyword: 'Famous Dominican Selection 1000 Torpedo',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Famous Dominican Selection 5000 Torpedo',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Famous Dominican Selection 5000 Toro',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Famous Dominican Selection 1000 Toro',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Famous Dominican Selection 4000 Torpedo',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Famous Dominican Selection 4000 Toro',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Famous Honduran Selection 2000 Petit Corona',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Stainless Steel Cigar Tube 6.5 Inches Long',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Guillotine Stainless Cigar Cutter 54 Ring',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Ironsides 25 Ebony Finish Oak',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Venetian Quilt - King by World Market King',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Jennifer Botanical Quilt: Multi - Cotton by World Market',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Sea Blue and Oasis Green Simone Reversible Quilt - Cotton - Queen by World Market Queen',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Indian Floral Alisha Quilt: Blue/Gray - Cotton - King Quilt by World Market King',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Coral and Oasis Green Simone Reversible Quilt - Queen by World Market Queen',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Blush Medallion Mariana Reversible Quilt: Pink - Cotton by World Market',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Aerin Floral Quilt: Pink - Cotton by World Market',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Gray Velvet Quilt by World Market',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Adari Paisley Border Reversible Quilt: Multi - Cotton by World Market',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Elephant Print Karima Reversible Quilt: Blue - Cotton by World Market',
+    merchant: '125441'
+  },
+  {
+    product_keyword: 'Comfort Foods: Rachael Ray\'s 30-Minute Meals (Hardcover) Book',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Rachael Ray 365: No Repeats',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Cooking \'Round The Clock: Rachael Ray 30-Minute Meals (Paperback) Book',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Get Togethers: Rachael Ray 30 Minute Meals (Paperback) Book',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Rachael Ray 2,4,6,8: Great Meals For Couples Or Crowds (Paperback) Book',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Rachael Ray Express Lane Meals: What To Keep On Hand, What To Buy Fresh For The Easiest-Ever 30-Minute Meals (Paperback) Book',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Rachael Ray\'s Look + Cook: 100 Can\'t-Miss Main Courses In Pictures (Paperback) Book',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Rachael Ray\'s Open House Cookbook: Over 200 Recipes For Easy Entertaining (Paperback) Book',
+    merchant: '124651'
+  },
+  {
+    product_keyword: 'Sur La Table Platinum Professional Round Cake Pan',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'Fat Daddioas Springform Pans',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'Fat Daddioas 10 Round Tube Pan',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'Wilton 5 Layer Cake Pan Set',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'Fat Daddioas Round Cake Pan',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'KitchenAidA Professional-Grade Nonstick Round Cake Pan',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'GreenPan Healthy Ceramic Nonstick Round Cake Pan',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'Fat Daddioas Round Cheesecake Pan',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'Sur La TableA Classic Chrome Duck Press',
+    merchant: '125427'
+  },
+  {
+    product_keyword: 'Sur La TableA Stainless Steel Fish Turners',
+    merchant: '125427'
+  }
+];
