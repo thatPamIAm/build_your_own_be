57a2edacfb2c02ad16d5a5368ab9b82ba1a734b1
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-19 09:22:51 -0600

    update file for eslint errors

--- db/migrations/20170517114658_renameForeignColumn.js
+++ db/migrations/20170517114658_renameForeignColumn.js
@@ -1,16 +1,15 @@
-
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return Promise.all([
-    knex.schema.table('products', function(table) {
+    knex.schema.table('products', function (table) {
       table.renameColumn('merchant_id', 'merchant');
     })
-  ])
+  ]);
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return Promise.all([
-    knex.schema.table('products', function(table) {
+    knex.schema.table('products', function (table) {
       table.renameColumn('merchant', 'merchant_id');
     })
-  ])
+  ]);
 };
