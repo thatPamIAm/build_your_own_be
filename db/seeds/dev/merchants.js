aeef743b15a36311c430628aa29673ac1f490eea
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-19 09:24:04 -0600

    update file for eslint errors

--- db/seeds/dev/merchants.js
+++ db/seeds/dev/merchants.js
@@ -1,24 +1,5 @@
-'use strict';
-var merchants = require('../../../merchants')
-var products = require('../../../products')
-
-exports.seed = function(knex, Promise) {
-  return knex('products').del()
-    .then(() => {
-      return knex('merchants').del()
-        .then(() => {
-          return knex('merchants').insert(merchants)
-          .then(() => {
-            var productPromises = [];
-            products.forEach((product) => {
-              let merchant = product.merchant;
-              productPromises.push(createProduct(knex, product, merchant))
-            });
-            return Promise.all(productPromises)
-          });
-        });
-    });
-};
+const merchants = require('../../../merchants');
+const products = require('../../../products');
 
 const createProduct = (knex, product, merchant) => {
   return knex('merchants').where('merchant_id', merchant).first()
@@ -26,6 +7,24 @@ const createProduct = (knex, product, merchant) => {
     return knex('products').insert({
       product_keyword: product.product_keyword,
       merchant: product.merchant
-    })
+    });
   });
+};
+
+exports.seed = function (knex, Promise) {
+  return knex('products').del()
+    .then(() => {
+      return knex('merchants').del()
+        .then(() => {
+          return knex('merchants').insert(merchants)
+          .then(() => {
+            const productPromises = [];
+            products.forEach((product) => {
+              const merchant = product.merchant;
+              productPromises.push(createProduct(knex, product, merchant));
+            });
+            return Promise.all(productPromises);
+          });
+        });
+    });
 };
--- db/seeds/test/merchants.js
+++ db/seeds/test/merchants.js
@@ -1,38 +1,38 @@
-exports.seed = function(knex, Promise) {
-  return knex ('products').del()
+exports.seed = function (knex, Promise) {
+  return knex('products').del()
     .then(() => knex('merchants').del())
     .then(() => {
       return Promise.all([
         knex('merchants').insert({
-          id:1, merchant_name: 'Foleys', merchant_id: '222222'
+          id: 1, merchant_name: 'Foleys', merchant_id: '222222'
         }, 'merchant_id')
         .then(merchant => {
           return knex('products').insert([
             {
               id: 1,
-              product_keyword: "Some really cool boots",
+              product_keyword: 'Some really cool boots',
               merchant: merchant[0]
             },
             {
               id: 2,
-              product_keyword: "Some sweet jacket",
+              product_keyword: 'Some sweet jacket',
               merchant: merchant[0]
             }
-          ])
+          ]);
         }),
         knex('merchants').insert({
-          id:2, merchant_name: 'Walmart', merchant_id: '111111'
+          id: 2, merchant_name: 'Walmart', merchant_id: '111111'
         }, 'merchant_id')
         .then(merchant => {
           return knex('products').insert([
             {
               id: 3,
-              product_keyword: "red t-shirt",
+              product_keyword: 'red t-shirt',
               merchant: merchant[0]
             },
             {
               id: 4,
-              product_keyword: "blue baseball cap",
+              product_keyword: 'blue baseball cap',
               merchant: merchant[0]
             }
           ]);
